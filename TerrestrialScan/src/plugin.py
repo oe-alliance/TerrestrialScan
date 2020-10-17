5f6aaa50a26cbef4a29357d6af00e1cd9a6433e1
Tony Whalley <tonywhalley@t-online.de>
2020-10-17 10:46:09 +0200

    python 3 change imports

--- TerrestrialScan/src/plugin.py
+++ TerrestrialScan/src/plugin.py
@@ -17,8 +17,8 @@ from Tools.BoundFunction import boundFunction
 
 from enigma import eComponentScan
 
-from TerrestrialScan import TerrestrialScan, setParams
-from MakeBouquet import MakeBouquet
+from .TerrestrialScan import TerrestrialScan, setParams
+from .MakeBouquet import MakeBouquet
 
 import os
 
